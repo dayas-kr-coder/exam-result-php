056dcde5b793f3c235b74c0defea8ffb3f32bbe4
Dayas K.R <dayaskr2005@gmail.com>
2024-01-20 01:19:21 +0530

    Add Font Awesome CDN

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php include_once('./templates/header.php'); ?>
 <title>Home</title>
+<?php include_once('./cdn/font-awesome.php'); ?>
 <?php include_once('./bootstrap/bootstrap.php'); ?>
 
 <?php include_once('./templates/footer.php'); ?>
